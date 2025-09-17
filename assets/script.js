a612a3ef56e075caf1c6de3e7ba982bcfdb8e668
jessicareslan <52937969+jessicareslan@users.noreply.github.com>
2025-09-17 21:52:58 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -5,5 +5,11 @@ document.querySelectorAll('nav .dropdown > a').forEach(link => {
       e.preventDefault();
       link.parentElement.classList.toggle('open');
     }
+    document.body.addEventListener("pointermove", (e)=>{
+  const { currentTarget: el, clientX: x, clientY: y } = e;
+  const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+  el.style.setProperty('--posX',  x-l-w/2);
+  el.style.setProperty('--posY',  y-t-h/2);
+})
   });
 });
