9f0e8e04718f85a2188a2c9e9a3240fec2fb567a
jessicareslan <52937969+jessicareslan@users.noreply.github.com>
2025-09-22 01:18:57 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -52,3 +52,10 @@ form.addEventListener("submit", (e) => {
   modal.style.display = "none";
 });
 
+document.body.addEventListener("pointermove", (e)=>{
+  const { currentTarget: el, clientX: x, clientY: y } = e;
+  const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+  el.style.setProperty('--posX',  x-l-w/2);
+  el.style.setProperty('--posY',  y-t-h/2);
+})
+
